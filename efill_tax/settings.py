8f95cba5772356daeb799be631f751c3edaf6268
nandini <nandini.soni8@gmail.com>
2016-12-20 11:10:44 +0530

    Removed pyc files and dbsqlite

--- efill_tax/settings.py
+++ efill_tax/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'efill_tax.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'efiil_db',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
