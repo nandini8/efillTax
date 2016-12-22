46179a3d8e3c6134b1ad49eeee615f61616b4fde
nandini <nandini.soni8@gmail.com>
2016-12-22 10:12:07 +0530

    divided unittests

--- efill_app/tests/test_models.py
+++ efill_app/tests/test_models.py
@@ -8,21 +8,6 @@ from django.db import IntegrityError, DataError
 
 # Create your tests here.
 
-class HomePageView(TestCase):
-
-	def test_home_page_template_rendered(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'ITRform.html')
-
-	def test_can_save_POST_requests_in_object(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['pan_number'] = 'AAAPL1234C'
-
-		response = itr_form_view(request)
-		self.assertEqual('AAAPL1234C',PersonalInfo.objects.first().pan_number)
-
-
 class PersonalInfoModelTest(TestCase):
 
 	def test_pan_should_be_unique(self):
---
+++ efill_app/tests/test_views.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.http import HttpRequest
+from efill_app.views import itr_form_view
+from efill_app.models import PersonalInfo
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError, DataError
+
+
+# Create your tests here.
+
+class HomePageView(TestCase):
+
+	def test_home_page_template_rendered(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'ITRform.html')
+
+	def test_can_save_POST_requests_in_object(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['pan_number'] = 'AAAPL1234C'
+
+		response = itr_form_view(request)
+		self.assertEqual('AAAPL1234C',PersonalInfo.objects.first().pan_number)
+
+
