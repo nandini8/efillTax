97f64ca5aac6e4df2618360c2f2df2d6e779eb43
sahil <chanchadsahil@gmail.com>
2017-01-02 18:34:12 +0530

    database on heroku, for login

--- efill_app/migrations/0001_initial.py
+++ efill_app/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import datetime
 
 
 class Migration(migrations.Migration):
@@ -14,9 +15,26 @@ class Migration(migrations.Migration):
             name='PersonalInfo',
             fields=[
                 ('pan_number', models.CharField(primary_key=True, max_length=10, serialize=False)),
-                ('first_name', models.CharField(max_length=25, blank=True)),
-                ('middle_name', models.CharField(max_length=25, blank=True)),
-                ('last_name', models.CharField(max_length=25)),
+                ('first_name', models.CharField(blank=True, max_length=25)),
+                ('middle_name', models.CharField(blank=True, max_length=25)),
+                ('last_name', models.CharField(default='', max_length=25)),
+                ('flat_no', models.CharField(default=' ', max_length=5)),
+                ('premise_name', models.CharField(blank=True, max_length=15)),
+                ('road_name', models.CharField(blank=True, max_length=10)),
+                ('area_name', models.CharField(default=' ', max_length=15)),
+                ('city_name', models.CharField(default=' ', max_length=15)),
+                ('state_name', models.CharField(default=' ', max_length=20)),
+                ('country_name', models.CharField(default=' ', max_length=20)),
+                ('pincode', models.CharField(default=' ', max_length=6)),
+                ('martial_status', models.CharField(default='Unmarried', max_length=10)),
+                ('dob', models.DateField(default=datetime.date.today)),
+                ('sex', models.CharField(default='Male', max_length=6)),
+                ('email_id', models.CharField(default=' ', max_length=25)),
+                ('mobile_1', models.CharField(default=' ', max_length=10)),
+                ('mobile_2', models.CharField(blank=True, max_length=10)),
+                ('std_code', models.CharField(blank=True, max_length=5)),
+                ('phone_no', models.CharField(blank=True, max_length=8)),
+                ('employer_category', models.CharField(default='OTH', max_length=3)),
             ],
         ),
     ]
